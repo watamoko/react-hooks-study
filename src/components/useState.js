bfdc57e23319709ea9eee45693f64326306137ba
watamoko <kei07161@yahoo.co.jp>
2020-11-17 22:35:45 +0900

    わざわざ関数使うuseStateを追加

--- src/components/useState.js
+++ src/components/useState.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 
+const foods = ['banana', 'apple', 'bread', 'rice', 'meat'];
+
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [food, setFood] = useState(0);
+
+  const OnRandomFoodsButtonClicked = () => {
+    setFood(Math.floor(Math.random()*foods.length));
+  }
 
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Counter</button>
+      <p>今日の晩御飯は {foods[food]}</p>
+      <button onClick={() => OnRandomFoodsButtonClicked()}>晩御飯ﾀｧｧｲﾑ</button>
     </div>
   );
 }
