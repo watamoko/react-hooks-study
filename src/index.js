25b94f6a402bc2d26e7d9640af3c2dcf3f376c55
watamoko <kei07161@yahoo.co.jp>
2020-11-17 22:24:46 +0900

    add useState

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Counter from './components/useState';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
